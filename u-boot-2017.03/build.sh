27f9fc67590f0f63947062d2e2b12b5eedc54b22
lei.zhan <lei.zhan@schneider-electric.com>
2017-11-08 13:11:47 +0800

    Modify the build script to make the build results into a folder
    Change-Id: Ie73edefd4a99cc1e5b1fde22b4176a6a67463eb6

--- u-boot-2017.03/build.sh
+++ u-boot-2017.03/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+BUILD_ROOT=`pwd`
+BUILD_OUTPUT_DIR=${BUILD_ROOT}/../_build
 CROSS_GCC_PATH=/opt/gcc-arm-none-eabi-5_4-2016q3/bin/
 export CROSS_COMPILE=${CROSS_GCC_PATH}arm-none-eabi-
 echo ${CROSS_COMPILE}
-make distclean
-make ok6410_defconfig
+make distclean O=${BUILD_OUTPUT_DIR}
+make ok6410_defconfig O=${BUILD_OUTPUT_DIR}
 #make rpi_3_32b_defconfig 
-make all V=1
+make all -j4 V=1 O=${BUILD_OUTPUT_DIR}
 
